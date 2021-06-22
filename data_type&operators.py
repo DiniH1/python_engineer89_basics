17344888b1caffde2964b14cf1f003e1c9067dd0
Dini Hassan <dinih51@gmail.com>
2021-06-22 12:04:25 +0100

    python_engineer

---
+++ data_type&operators.py
@@ -0,0 +1,37 @@
+# What are data types and operators
+# bool, numbers and strings
+# operators: arithmatic
+
+# - + / * %
+# - used to subtract 2 val
+# used to add 2 val
+# * multiply
+# / divide
+# % gives the remainder
+
+# comparison operators
+# >
+# <
+# ==
+# !=
+# <=
+# >=
+
+# number_1 = 4
+# number_2 = 2
+
+print(number_1+number_2) # add two vals
+print(number_1-number_2) # subtract two vals
+print(number_1*number_2) #multiply
+print(number_1/number_2) #divide
+print(number_1%number_2) #remainder
+print(number_1**number_2)
+number_1 = 4
+number_2 = 2
+
+# lets look at bool vals
+print(number_1 == number_2) # is number1 == to number 2 (True/False)
+print(number_1!=number_2) # is num1 not equal to num2
+print(number_1>=number_2)
+print(number_1<=number_2)
+
---
+++ variables.py
@@ -0,0 +1,63 @@
+# lets test
+print("Hello Dini H")
+#print func used to display outcome provided in the string
+
+#Variables
+#python variables as a place holder to store data
+# it could me a string "anything between these quotations"
+# integers/numbers
+#Syntax to create a variable name of the variable = value of the variable
+#foolow your logical naming convention
+
+# First_Name = 'Dini'
+# Last_Name = 'Hassan'
+# #lets create val to store int val
+# Salary = 10.5 #float val contains decimal
+# age = 19 #int value
+# my_age = '22'
+
+
+
+
+#print(First_Name)
+#print(Last_Name)
+#print(Salary)
+#print(age)
+#print(my_age)
+#type(age) helps us find the type of variable
+#print(type(age))
+#print(type(my_age))#will
+
+#input() is python to interact with user to ask user required data
+# user_name = input("Please enter your name ")
+# print('Hello ')
+#
+# print(user_name)
+
+# Ativity
+# variables first_name, last_name, age, DOB
+#prompt user is input above below
+#print/display the type of each val recieved from user
+#then display the data back to the user with greeting message
+
+
+# Activity/task
+#
+# variables first_name, last_name, age, DOB
+# prompt user to input above value
+# print/display the type of each value received from the user
+# then display the data back to user with greeting message
+
+first_name = input("Please enter your first name ")
+last_name = input("Please enter your last name ")
+age = input("What is your age? ")
+DOB = input("What is your Date of birth? ")
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(DOB))
+print("Hello")
+print(first_name)
+print(last_name)
+print(age)
+print(DOB)
